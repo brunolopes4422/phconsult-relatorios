beb8ab7335fad6fef4ad934d57719f880c840df2
Bruno Lopes <brunolopes4422@gmail.com>
2026-05-14 15:13:43 -0300

    fix oauth popup e authcallback

--- frontend/src/pages/AuthCallback.jsx
+++ frontend/src/pages/AuthCallback.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 import { useSearchParams, useNavigate } from 'react-router-dom'
-import api from '../lib/auth'
+import api from '../lib/api'
 
 export default function AuthCallback() {
   const [params] = useSearchParams()
